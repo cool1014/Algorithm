f4b0dd4c0b3b51419968b8109e4fd03cf1a6ab17
cool1014 <dnwntjs009@gmail.com>
2026-05-31 09:05:57 +0900

    ëSWEA 2072 홀수만 더하기

---
+++ src/SWEA/D1/T_2072.java
@@ -0,0 +1,38 @@
+package SWEA.D1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * [SWEA] 2072. 홀수만 더하기
+ *
+ * 시간복잡도 : O(T)
+ * - 내부는 10번만 고정으로 돌기 때문에 O(1)이고 전체는 O(T * 10) = O(T)가 됨
+ *
+ * 풀이 과정
+ * - 10번 루프를 돌며 홀수 일때만 sum에 누적
+ */
+public class T_2072 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= T; tc++) {
+            int sum = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            for (int i = 0; i < 10; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                if (num % 2 == 1) {
+                    sum += num;
+                }
+            }
+            System.out.println("#" + tc + " " + sum);
+
+
+        }
+    }
+}
